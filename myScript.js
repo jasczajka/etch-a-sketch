0c381d93e5b92185106a66226ec048bc4f2fe17d
jasczajka <czajka.jan12@gmail.com>
2022-01-09 11:15:10 +0100

    add a resize functionality

--- myScript.js
+++ myScript.js
@@ -1,21 +1,43 @@
 //create 16 divs and add them to  container
 //let canvasSize = prompt("how wide should the canvas be?");
-let canvasSize = 16;
+const defaultSize = 16;
 const canvas = document.querySelector('#canvas');
-console.log(canvas);
-for (let i = 0; i < Math.pow(canvasSize, 2); i++)
-{
-  let newDiv = document.createElement('div');
-  newDiv.classList.add('pixel');
-  canvas.appendChild(newDiv);
+
+
+function drawCanvas(canvasSize) {
+  for (let i = 0; i < Math.pow(canvasSize, 2); i++)
+  {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('pixel');
+    canvas.appendChild(newDiv);
+  }
+  canvas.style.gridTemplate = `repeat(${canvasSize}, 1fr) / repeat(${canvasSize}, 1fr)`;
+  addDrawingFunctionality()
 }
+
+
 //drawing functionality
-let pixels = document.querySelectorAll('.pixel');
-for (let i = 0; i < pixels.length; i++)
+function addDrawingFunctionality ()
 {
-  pixels[i].addEventListener('mouseover', (e) => {e.target.style.backgroundColor = 'black' });
+  let pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i++)
+  {
+    pixels[i].addEventListener('mouseover', (e) => {e.target.style.backgroundColor = 'black' });
+  }
 }
+
+drawCanvas(defaultSize);
+
 //reset button functionality
 document.querySelector('#reset').addEventListener('click', () =>{
+  let pixels = document.querySelectorAll('.pixel');
   Array.from(pixels).forEach(pixel => pixel.style.backgroundColor = 'white');
+  });
+
+document.querySelector('#reset').addEventListener('click', () => {
+  //remove the current canvas
+  let pixels = document.querySelectorAll('.pixel');
+  Array.from(pixels).forEach(pixel => pixel.remove());
+  //draw a new canvas
+  drawCanvas(prompt('New size of the canvas?'));
 });

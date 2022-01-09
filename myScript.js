b987fe0b91d5f0d6fd3b2258126f11ea6d947785
jasczajka <czajka.jan12@gmail.com>
2022-01-09 14:12:39 +0100

    add rainbow mode and pencil mode buttons

--- myScript.js
+++ myScript.js
@@ -4,38 +4,47 @@ const defaultSize = 16;
 const canvas = document.querySelector('#canvas');
 
 
-function drawCanvas(canvasSize) {
+function drawCanvas(canvasSize, drawingColor) {
   for (let i = 0; i < Math.pow(canvasSize, 2); i++)
   {
     let newDiv = document.createElement('div');
     newDiv.classList.add('pixel');
-    newDiv.style.height = `calc(70vh / ${canvasSize})`;
-    newDiv.style.width = `calc(70vh / ${canvasSize})`;
+    newDiv.style.height = `calc(60vh / ${canvasSize})`;
+    newDiv.style.width = `calc(60vh / ${canvasSize})`;
     canvas.appendChild(newDiv);
   }
   canvas.style.gridTemplate = `repeat(${canvasSize}, 1fr) / repeat(${canvasSize}, 1fr)`;
-  addDrawingFunctionality()
+  addDrawingFunctionality(drawingColor)
 }
 
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 //drawing functionality
-function addDrawingFunctionality ()
+function addDrawingFunctionality(drawingColor)
 {
   let pixels = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixels.length; i++)
   {
-    pixels[i].addEventListener('mouseover', (e) => {e.target.style.backgroundColor = 'black' });
+    pixels[i].addEventListener('mouseenter', (e) => {e.target.style.backgroundColor = drawingColor });
   }
 }
 
-drawCanvas(defaultSize);
+drawCanvas(defaultSize, 'black');
 
-//reset button functionality
+//clear current canvas
 document.querySelector('#reset').addEventListener('click', () =>{
   let pixels = document.querySelectorAll('.pixel');
   Array.from(pixels).forEach(pixel => pixel.style.backgroundColor = 'white');
 });
 
+//new canvas with new size
 document.querySelector('#new-canvas').addEventListener('click', () => {
   //remove the current canvas
   let pixels = document.querySelectorAll('.pixel');
@@ -46,5 +55,29 @@ document.querySelector('#new-canvas').addEventListener('click', () => {
     newSize = prompt('Please enter new length of the canvas (max 100)');
   }
   while(isNaN(newSize) === true || newSize < 0 || newSize > 100)
-  drawCanvas(newSize);
+  drawCanvas(newSize, 'black');
+});
+
+
+let pencilButton = document.querySelector('#pencil-mode');
+let rainbowButton = document.querySelector('#rainbow-mode');
+
+//rainbow button
+rainbowButton.addEventListener('click', () => {
+  rainbowButton.style.border = '3px solid #FF968A';
+  pencilButton.style.border = '2px solid';
+  let pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i++)
+  {
+    pixels[i].addEventListener('mouseenter', (e) => {e.target.style.backgroundColor = getRandomColor() });
+  }
+});
+
+
+//pencil button
+pencilButton.addEventListener('click', () => {
+  pencilButton.style.border = '3px solid #FF968A';
+  rainbowButton.style.border = '2px solid';
+  addDrawingFunctionality('black');
+
 });
